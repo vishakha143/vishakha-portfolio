c391ffeade07cb6a33588f2e67545bbda5f254e7
vishakha143 <158565550+vishakha143@users.noreply.github.com>
2026-09-16 15:17:27 +0530

    Replace Github icon with FaGithub icon

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,5 +1,6 @@
-import { ArrowLeft, ArrowUpRight, Github } from "lucide-react";
+import { ArrowLeft, ArrowUpRight} from "lucide-react";
 import { projects } from "@/sections/Projects";
+import { FaGithub} from "react-icons/fa";
 
 export const ProjectsPage = () => {
   return (
@@ -91,7 +92,7 @@ export const ProjectsPage = () => {
                         rel="noopener noreferrer"
                         className="inline-flex items-center gap-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors"
                       >
-                        <Github className="w-4 h-4" />
+                        < FaGithub className="w-4 h-4" />
                         Source Code
                       </a>
                     </div>
