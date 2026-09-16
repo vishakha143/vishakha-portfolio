1bf39258ae81e51631e2ad9e48be29108d932e76
vishakha143 <158565550+vishakha143@users.noreply.github.com>
2026-09-16 14:25:55 +0530

    Route projects page from portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ProjectsPage } from "./pages/ProjectsPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const Root = window.location.pathname === "/projects" ? ProjectsPage : App;
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
-)
+);
